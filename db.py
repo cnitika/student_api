ee713d05a53f79ee782e5dc18de715fd37d7e8d2
cnitika <cnitika040@gmail.com>
2026-07-16 12:01:53 +0000

    Secured credentials using .env and added .gitignore

--- db.py
+++ db.py
@@ -1,11 +1,15 @@
 import pymysql
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_connection():
     return pymysql.connect(
-        host="host.docker.internal",
-        user="root",
-        password="2810Nitik@",
-        database="student_db",
+        host=os.environ.get("DB_HOST", "127.0.0.1"),
+        user=os.environ.get("DB_USER", "root"),
+        password=os.environ.get("DB_PASSWORD"),
+        database=os.environ.get("DB_NAME", "student_db"),
         port=3306,
         cursorclass=pymysql.cursors.DictCursor
     )
